55a8f8e621a6da309e29b794d543e7e534a85fd7
kavyasontanavar123 <kavyasontanavar@gmail.com>
2024-12-05 15:26:13 +0530

    creating first git hub project

---
+++ src/main/java/com/example/fgp/FirstGithubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGithubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGithubProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fgp/FirstGithubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGithubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
